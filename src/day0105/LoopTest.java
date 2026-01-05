3d34da3c95b57a4512ff26aba4e63dc488f51f15
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2026-01-05 13:02:39 +0900

    클래스명변경 For->Loop 2026-01-05

---
+++ src/day0105/LoopTest.java
@@ -0,0 +1,22 @@
+package day0105;
+
+public class LoopTest {
+
+	public static void main(String[] args) {
+		
+		// Loop  : 반복문의 통칭(for, While, do-while문)
+		
+		//for문
+		for(int i=1; i<=5; i++) {
+			System.out.println("월요일 이네요");
+		}
+		
+		for(int i=1; i<=5; i++) {
+			System.out.println("맛점하세용");
+		}
+		
+		
+		
+	}
+
+}
