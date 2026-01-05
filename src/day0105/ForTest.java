41b6bf9fe838397d6805b98dfca6e587258dd932
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2026-01-05 15:38:58 +0900

    미션_일부내용수정_2026-01-05

--- src/day0105/ForTest.java
+++ src/day0105/ForTest.java
@@ -3,30 +3,31 @@ package day0105;
 public class ForTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("문제 1 ");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
 		}
 
 		System.out.println("=========================");
+		System.out.println("문제 2 ");
 		for (int i = 1; i <= 5; i++) {
 			System.out.print(i + " ");
 		}
 		System.out.println();
 		System.out.println("=========================");
-
+		System.out.println("문제 3 ");
 		for (int i = 5; i >= 1; i--) {
 			System.out.print(i + " ");
 		}
 		System.out.println();
 		System.out.println("=========================");
-		
+		System.out.println("문제 4 ");
 		for (int i = 1; i <= 4; i++) {
 			System.out.print(i * 3 + " ");
 		}
 		System.out.println();
 		System.out.println("=========================");
-
+		System.out.println("문제 5 ");
 		System.out.print("[");
 		for (int i = 1; i <= 4; i++) {
 		    System.out.print(i * 5);
