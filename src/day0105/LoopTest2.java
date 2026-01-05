5d723ffc2266d1da3c25079b0fb21e44a30db855
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2026-01-05 14:57:05 +0900

    2025-01-26 while 문 수정

--- src/day0105/LoopTest2.java
+++ src/day0105/LoopTest2.java
@@ -7,15 +7,16 @@ public class LoopTest2 {
 
 		int i = 1;
 
-		while (true) {
+		while (i<=5) {
 			System.out.println(i);
 			i++; // 후위 연산자로 해당 증감식 없으면 무한루프!!!!!
-
-			if (i <= 5) {
-				continue;
-			} else {
-				break;
-			}
+//
+//			if (i <= 5) {
+//				continue; // while 내의 continue 제어문은 조건식으로 분기
+//							// for내의 continue 증감자로 분기
+//			} else {
+//				break; //break를 감싸는 가장 가까운 반복문 또는 switch문을 벗어나는 역할
+//			}
 		}
 
 	}
